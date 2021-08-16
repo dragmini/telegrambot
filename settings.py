a1144c27b6dc97bbc1e7a48ad2bffdebfae4c163
dragmini <88884304+dragmini@users.noreply.github.com>
2021-08-16 06:52:17 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-token = '1962147532:AAENfX9y-eBiQqRcrg62Ekghdhip8mWdsxY'    # Token telegram bot
+token = '123'    # Token telegram bot
 
-api_weather = '74cfd40b-1e12-4cac-8379-21e3b4477904'    # Token weather yandex
-api_list = '9005cf90-0aa6-431d-9480-124ee21b6cee'   # Token list yandex
-api_weather_openweather = 'cac8f5b67a00d4b4cb1c5f18d2c23b22'
+api_weather = '123'    # Token weather yandex
+api_list = '123'   # Token list yandex
+api_weather_openweather = '123'
